f68d337bf9971d550adb750fc1ff945d85f10361
Piero Giusti <pierophp@gmail.com>
2020-04-15 14:53:23 -0300

    Fix main

--- display.py
+++ display.py
@@ -43,8 +43,8 @@ class Sizes(object):
 
     PIECE        = 45
     BUNNY_PICKER = 70
-    CARD_WIDTH   = 75
-    CARD_HEIGHT  = 100
+    CARD_WIDTH   = 50
+    CARD_HEIGHT  = 75
     YOUR_TURN    = 24
     TOKEN        = 80
 
